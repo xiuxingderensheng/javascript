59ccf22870481238d4934c8733cfa52ddf212d02
xiuxingderensheng <543621754@qq.com>
2017-11-20 16:53:38 +0800

    add a comment

--- webpack_test/webpack.config.js
+++ webpack_test/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	devtool: "eval-source-map",
 
-	entry: __dirname + "/app/main.js",
+	entry: __dirname + "/app/main.js",//__dirname是一个nodejs的全局变量，便是项目根目录
 	output: {
 		path: __dirname + "/public",
 		filename: "bundle.js"
