84e2daf3ce93de3e387b68415cdb9c71ac19b04b
xiuxingderensheng <543621754@qq.com>
2017-12-04 08:47:49 +0800

    use webpack's loader and use react and bable

--- webpack_test/webpack.config.js
+++ webpack_test/webpack.config.js
@@ -1,15 +1,36 @@
 module.exports = {
-	devtool: "eval-source-map",
+	devtool: "eval-source-map",//eval-source-map适用于开发阶段，生产阶段别用
 
-	entry: __dirname + "/app/main.js",//__dirname是一个nodejs的全局变量，便是项目根目录
+	//js打包配置
+	entry: __dirname + "/app/main.js",//__dirname是一个nodejs的全局变量，指向当前执行脚本所在的目录
 	output: {
 		path: __dirname + "/public",
 		filename: "bundle.js"
 	},
 
+	//开发服务器配置
 	devServer: {
 		contentBase: __dirname + "/public",
 		historyApiFallback: true,
 		inline: true
+	},
+
+	//loaders配置
+	module: {
+		rules: [
+			{
+				test: /\.json$/,
+				use: {
+					loader: "json-loader"
+				}
+			},
+			{
+				test: /(\.jsx||\.js)$/,//匹配loaders所处理文件的拓展名的正则表达式（必须）
+				use: {
+					loader: "babel-loader"//loader的名称
+				},
+				exclude: /node_modules/
+			}
+		]
 	}
 }
